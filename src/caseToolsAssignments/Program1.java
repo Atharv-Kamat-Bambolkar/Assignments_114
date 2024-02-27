6a5f0a79e7b413d96c16beed18d9c9e04869455e
Atharv Kamat Bambolkar <atharvkamatbambolkar2@gmail.com>
2024-02-27 15:53:33 +0530

    First commit

---
+++ src/caseToolsAssignments/Program1.java
@@ -0,0 +1,17 @@
+package caseToolsAssignments;
+
+import java.util.Scanner;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = a + b;
+		System.out.println("c = " + c);
+	}
+
+}
