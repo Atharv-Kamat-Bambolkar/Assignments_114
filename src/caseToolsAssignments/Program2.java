e1e8f77c694296baa5b02e38cd1394730aaa5ea6
Atharv Kamat Bambolkar <atharvkamatbambolkar2@gmail.com>
2024-02-27 17:06:24 +0530

    Third commit

---
+++ src/caseToolsAssignments/Program2.java
@@ -0,0 +1,19 @@
+package caseToolsAssignments;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int factorial = 1;
+		for (int i = 1; i <= num; i++) {
+			factorial *= i;
+		}
+		System.out.println("Factorial of a number = " + factorial);		
+	}
+
+}
