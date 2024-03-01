43d6c80bfacdccff2865da62bb67bf0ae24bf870
Atharv Kamat Bambolkar <atharvkamatbambolkar2@gmail.com>
2024-03-01 16:26:14 +0530

    Second commit

--- src/caseToolsAssignments/Program3.java
+++ src/caseToolsAssignments/Program3.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Program3 {
 
-	static void printFib(int num) {
+	public static void printFib(int num) {
 		if (num < 1) {
 			System.out.println("Invalid Number of terms");
 			return;
@@ -28,12 +28,39 @@ public class Program3 {
 		}
 	}
 	
+	public static void printFib1(int num2) {
+		if (num2 < 1) {
+			System.out.println("Invalid Number of terms");
+			return;
+		}
+		
+		int prev1 = 1;
+		int prev2 = 0;
+		
+		for (int i = 1; i <= num2; i++) {
+			if (i > 2) {
+				int num1 = prev1 + prev2;
+				prev2 = prev1;
+				prev1 = num1;
+				System.out.print(num1 + " ");
+			}
+			
+			if (i == 1)
+				System.out.print(prev2 + " ");
+			if (i == 2)
+				System.out.println(prev1 + " ");
+		}
+
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		try (Scanner sc = new Scanner(System.in)) {
 			int num = sc.nextInt();
+			int num2 = sc.nextInt();
 			printFib(num);
+			printFib1(num2);
 		}
 		
 	}
