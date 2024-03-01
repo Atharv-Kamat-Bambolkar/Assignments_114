35e307bf48d6f3a608c22cfa0089cb1d7b435eb1
Atharv Kamat Bambolkar <atharvkamatbambolkar2@gmail.com>
2024-03-01 16:16:14 +0530

    First commit

--- src/caseToolsAssignments/Program1.java
+++ src/caseToolsAssignments/Program1.java
@@ -7,15 +7,16 @@ public class Program1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int d = sc.nextInt();
-		int e = sc.nextInt();
-		int c = add(a, b);
-		int f = add1(d, e);
-		System.out.println("c = " + c);
-		System.out.println("f = " + f);
+		try (Scanner sc = new Scanner(System.in)) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int d = sc.nextInt();
+			int e = sc.nextInt();
+			int c = add(a, b);
+			int f = add1(d, e);
+			System.out.println("c = " + c);
+			System.out.println("f = " + f);
+		}
 	}
 
 	public static int add(int a, int b) {
