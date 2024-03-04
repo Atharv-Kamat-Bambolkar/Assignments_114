f1b0652e2d90d434333b515a06095136c266ae3b
Atharv Kamat Bambolkar <atharvkamatbambolkar2@gmail.com>
2024-03-04 16:00:10 +0530

    First commit

---
+++ src/caseToolsAssignments/Program6.java
@@ -0,0 +1,23 @@
+package caseToolsAssignments;
+
+public class Program6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Person p1 = new Person();
+		Person p2 = new Person();
+		
+		p1.setName("Atharv");
+		p1.setAge(20);
+		p2.setName("Aaron");
+		p2.setAge(20);
+		
+		System.out.println("Name = " + p1.getName());
+		System.out.println("Age = " + p1.getAge());
+		System.out.println("Name = " + p2.getName());
+		System.out.println("Age = " + p2.getAge());
+		
+	}
+
+}
